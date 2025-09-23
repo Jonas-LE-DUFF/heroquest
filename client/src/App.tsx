e74272c7ff110c26704bdb1dcd30f474ceb17786
jonas LE DUFF <jonasleduff14@gmail.com>
2025-09-23 10:55:36 +0200

    form to join game

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { io, Socket } from 'socket.io-client';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { io, Socket } from "socket.io-client";
+import "./App.css";
+import { wait } from "@testing-library/user-event/dist/utils";
+import { eventNames } from "process";
 
 function App() {
   const [socket, setSocket] = useState<Socket | null>(null);
   const [connected, setConnected] = useState(false);
+  const [pending, setPending] = useState(false);
+  const [playerName, setPlayerName] = useState("");
+  const [gameId, setGameId] = useState("");
 
   useEffect(() => {
-    const newSocket = io('http://localhost:5000');
+    const newSocket = io("http://localhost:5000");
     setSocket(newSocket);
 
-    newSocket.on('connect', () => {
+    newSocket.on("connect", () => {
       setConnected(true);
-      console.log('Connecté au serveur');
+      console.log("Connecté au serveur");
     });
 
     return () => {
@@ -20,14 +25,48 @@ function App() {
     };
   }, []);
 
+  function handleJoinGame() {
+    setPending(true);
+    wait(30000);
+    const playerName = (
+      document.getElementById("playerName") as HTMLInputElement
+    ).value;
+    const gameId = (document.getElementById("gameId") as HTMLInputElement)
+      .value;
+    if (socket) {
+      socket.emit("join-game", gameId, playerName);
+    }
+    setPending(false);
+  }
+
+  function isButtonDisabled() {
+    return !pending && (playerName.trim() === "" || gameId.trim() === "");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hero Quest</h1>
-        <p>Status: {connected ? 'Connecté' : 'Déconnecté'}</p>
-        <button onClick={() => socket?.emit('join-game', 'test-game', 'JoueurTest')}>
-          Rejoindre une partie
-        </button>
+        <p>Status: {connected ? "Connecté" : "Déconnecté"}</p>
+        <form action={handleJoinGame}>
+          <input
+            onChange={(event) => setPlayerName(event.target.value)}
+            id="playerName"
+            type="text"
+            placeholder="Nom du joueur"
+            content={playerName}
+          />
+          <input
+            onChange={(event) => setGameId(event.target.value)}
+            id="gameId"
+            type="text"
+            placeholder="ID de la partie"
+            content={gameId}
+          />
+          <button type="submit" disabled={isButtonDisabled()}>
+            {pending ? "Chargement ..." : "Rejoindre une partie"}
+          </button>
+        </form>
       </header>
     </div>
   );
