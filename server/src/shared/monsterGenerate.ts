d24bb79277788df0f2349a88bead6b0aad597184
Jonas Le Duff <jonas.leduff@etu.unicaen.fr>
2025-12-08 15:11:07 +0100

    monster stats done

--- server/src/shared/monsterGenerate.ts
+++ server/src/shared/monsterGenerate.ts
@@ -1,4 +1,5 @@
 import { Monster, monsterClass, Unit } from "./type";
+import monsterStats from "./stats/monsters.json"
 
 function generateMonster(monsterId: string, monsterType: monsterClass) {
   const stats: Unit = getMonsterStats(monsterType);
@@ -12,14 +13,29 @@ function generateMonster(monsterId: string, monsterType: monsterClass) {
 }
 
 function getMonsterStats(monsterType: monsterClass): Unit {
+  const monster = monsterStats.find((monster) => {
+    return monster.id === monsterType;
+  });
+  if (!monster) {
+    console.error(`Monster stats not found for type: ${monsterClass[monsterType]}`);
+    return {
+      name: monsterClass[monsterType],
+      hp: 1,
+      maxHp: 1,
+      spiritPoints: 1,
+      nbAttackDice: 1,
+      nbDefenseDice: 1,
+      movements: 1,
+    };
+  }
   return {
     name: monsterClass[monsterType],
-    hp: 1,
-    maxHp: 1,
-    spiritPoints: 1,
-    nbAttackDice: 1,
-    nbDefenseDice: 1,
-    movements: 10,
+    hp: monster.health,
+    maxHp: monster.health,
+    spiritPoints: monster.spiritPoints,
+    nbAttackDice: monster.nbAttackDice,
+    nbDefenseDice: monster.nbDefenseDice,
+    movements: monster.movements,
   };
 }
 
