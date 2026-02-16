c055b25686b83f51892a22db021475018b266ec8
Jonas Le Duff <jonas.leduff@etu.unicaen.fr>
2026-02-16 12:28:27 +0100

    format : prettier doing modifs

--- server/src/POO/classes/Factories/HeroFactory.ts
+++ server/src/POO/classes/Factories/HeroFactory.ts
@@ -1,5 +1,4 @@
 import { HeroCategory } from "../../enums/Categories/HeroCategory";
-import { Position } from "../Position/Position";
 import { Hero } from "../Units/Hero";
 import { Stats } from "../Units/Stats";
 
@@ -10,59 +9,53 @@ import { HeroCreationWish } from "../FromClient/HeroCreationWish";
 import { getSpellsForElements } from "../../../services/SpellService";
 
 class HeroFactory {
-    createHero(gameId: string, heroCreationWish: HeroCreationWish): Hero | null {
+  createHero(gameId: string, heroCreationWish: HeroCreationWish): Hero | null {
+    const id: string = randomUUID();
 
-        const id : string = randomUUID();
+    const heroCategory: HeroCategory = heroCreationWish.heroCategory;
 
-        const heroCategory : HeroCategory = heroCreationWish.heroCategory;
+    const stats: Stats = getHeroBaseStats(heroCategory);
 
-        const stats: Stats = getHeroBaseStats(heroCategory);
+    const equipment: Equipment = getHeroStartingEquipment(heroCategory);
 
-        const equipment: Equipment = getHeroStartingEquipment(heroCategory);
+    const spells = getSpellsForElements(gameId, heroCreationWish.spellElements);
 
-        const spells = getSpellsForElements(gameId, heroCreationWish.spellElements);
-
-        const hero =  new Hero(
-            id,
-            heroCreationWish.name,
-            heroCategory,
-            stats,
-            equipment,
-        );
-        hero.spells = spells;
-        return hero;
-    };
-}
-
-function getHeroBaseStats(heroType: HeroCategory) : Stats{
-  const heroData = heroStats.find(
-    (hero) => hero.id === heroType
-  );
-    if (!heroData) {
-        throw new Error(`Hero data not found for heroType: ${heroType}`);
-    }
-    return {
-        health: heroData.health,
-        maxHealth: heroData.health,
-        spirit: heroData.spiritPoints,
-        nbDefenseDice: heroData.nbDefenseDice,
-        movements: heroData.movements,
-    };
-}
-
-function getHeroStartingEquipment(heroType: HeroCategory) : Equipment{
-    const heroData = heroStats.find(
-      (hero) => hero.id === heroType
+    const hero = new Hero(
+      id,
+      heroCreationWish.name,
+      heroCategory,
+      stats,
+      equipment,
     );
-        if (!heroData) {
-            throw new Error(`Hero data not found for heroType: ${heroType}`);
-        }
-        const equipment = new Equipment(0);
-        heroData.equipments.forEach((item) => {
-            equipment.addEquipmentById(item);
-        });
-        return equipment;
+    hero.spells = spells;
+    return hero;
+  }
 }
 
+function getHeroBaseStats(heroType: HeroCategory): Stats {
+  const heroData = heroStats.find((hero) => hero.id === heroType);
+  if (!heroData) {
+    throw new Error(`Hero data not found for heroType: ${heroType}`);
+  }
+  return {
+    health: heroData.health,
+    maxHealth: heroData.health,
+    spirit: heroData.spiritPoints,
+    nbDefenseDice: heroData.nbDefenseDice,
+    movements: heroData.movements,
+  };
+}
+
+function getHeroStartingEquipment(heroType: HeroCategory): Equipment {
+  const heroData = heroStats.find((hero) => hero.id === heroType);
+  if (!heroData) {
+    throw new Error(`Hero data not found for heroType: ${heroType}`);
+  }
+  const equipment = new Equipment(0);
+  heroData.equipments.forEach((item) => {
+    equipment.addEquipmentById(item);
+  });
+  return equipment;
+}
 
 export { HeroFactory };
